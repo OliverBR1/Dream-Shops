dbd81c89552946061e3770a808bb2acc0a862d82
OliverBR1 <Br.0liveira@outlook.com>
2024-10-31 23:18:10 -0300

    first commit

---
+++ src/main/java/com/dailycodework/dream_shops/service/order/IOrderService.java
@@ -0,0 +1,12 @@
+package com.dailycodework.dream_shops.service.order;
+
+import com.dailycodework.dream_shops.dto.OrderDto;
+import com.dailycodework.dream_shops.model.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order placeOrder(Long userId);
+    OrderDto getOrder(Long orderId);
+    List<OrderDto> getUserOrders(Long userId);
+}
